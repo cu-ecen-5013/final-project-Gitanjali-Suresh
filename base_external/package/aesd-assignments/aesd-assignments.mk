743de71b3f1c077e10ad8bb6aae75baf9116ce76
Gitanjali Suresh <gitanjali.suresh05@gmail.com>
2020-04-09 04:21:52 +0000

    Tester socket server added

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = '700402b27cb887f9f6a6b5dd339c840abfbe3f90'
+AESD_ASSIGNMENTS_VERSION = '76c7a2d98043994251cae6940d70c71c9a8624db'
 AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-5013/final-project-sarayumanagoli.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 
@@ -17,6 +17,7 @@ endef
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/helloworld $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/sharedmem $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/server $(TARGET_DIR)/bin
 endef
 
 
