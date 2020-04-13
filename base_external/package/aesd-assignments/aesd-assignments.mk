e5aac014046a52f3ad9a424d3d545fe4cd372917
Gitanjali-Suresh <gitanjali.suresh05@gmail.com>
2020-04-12 19:26:06 -0600

    Socket implementation test complete

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = '0f7cf04d95ace91f2d94462488c5cd169d837588'
+AESD_ASSIGNMENTS_VERSION = 'b332878d14d00e7d6455002c8bf42f0cddca4450'
 AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-5013/final-project-sarayumanagoli.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 
@@ -18,6 +18,8 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/helloworld $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/sharedmem $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/server $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/servertest $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/clienttest $(TARGET_DIR)/bin
 endef
 
 
