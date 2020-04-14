63e06f2d2482c917e569499a6564044218dacd9b
Gitanjali-Suresh <gitanjali.suresh05@gmail.com>
2020-04-14 01:32:29 -0600

    TMP102 works with BBB

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = '0fe5439853605398e2c446b2f6503884a99e42b1'
+AESD_ASSIGNMENTS_VERSION = 'c41098ff69033855b9057a7208c518f1fd3d20a9'
 AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-5013/final-project-sarayumanagoli.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 
@@ -20,6 +20,8 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/server $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/servertest $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/clienttest $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/TMP102 $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/gassensor $(TARGET_DIR)/bin
 endef
 
 
