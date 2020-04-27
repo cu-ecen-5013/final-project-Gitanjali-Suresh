2827345b05f786533a7c8fc039ec92e50c1e5c2d
Gitanjali-Suresh <gitanjali.suresh05@gmail.com>
2020-04-27 01:21:16 -0600

    Added start stop script

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,11 +5,7 @@
 #
 ##############################################################
 
-<<<<<<< HEAD
-AESD_ASSIGNMENTS_VERSION = 'a987198d552d27ffcc9a15340657a04ba9a31f61'
-=======
-AESD_ASSIGNMENTS_VERSION = '44760558d562c1f5d7cb5c99c56e98f0912b790d'
->>>>>>> 70ebff34ef2a8bdbb3e9598b215baa069496cc17
+AESD_ASSIGNMENTS_VERSION = 'be2a5339b5df152f21df024db99b7bf32231369d'
 AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-5013/final-project-sarayumanagoli.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 
@@ -21,9 +17,10 @@ endef
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/producer $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/consumer $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/servertest $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/servertest $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/clienttest $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/receiver $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/SocketServer_Integrated/server-start-stop.sh $(TARGET_DIR)/etc/init.d/S99server
 endef
 
 
