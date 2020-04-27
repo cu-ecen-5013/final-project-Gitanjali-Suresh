791685a6726dd29f05dc8cfd53d6aa2e63467b7d
sarayumanagoli <sarayumanagoli@gmail.com>
2020-04-27 01:55:54 -0600

    Restructed repository commit

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = '44760558d562c1f5d7cb5c99c56e98f0912b790d'
+AESD_ASSIGNMENTS_VERSION = 'f0acec81e6f1c6c1a29b061dce55da9b0069b3d1'
 AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-5013/final-project-sarayumanagoli.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 
@@ -15,13 +15,13 @@ define AESD_ASSIGNMENTS_BUILD_CMDS
 endef
 
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/helloworld $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/sharedmem $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/server $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/servertest $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/producer $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/consumer $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/servertest $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/clienttest $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/TMP102 $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/gassensor $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/receiver $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/SocketServer_Integrated/server-start-stop.sh $(TARGET_DIR)/etc/init.d/S99server
+	$(INSTALL) -m 0755 $(@D)/SocketClient_Integrated/client-start-stop.sh $(TARGET_DIR)/etc/init.d/S98client
 endef
 
 
