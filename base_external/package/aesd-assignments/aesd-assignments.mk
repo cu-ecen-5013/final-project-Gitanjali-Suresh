663779f2725d712f6b2c310271b8a7cebe766e37
Gitanjali-Suresh <gitanjali.suresh05@gmail.com>
2020-04-26 22:13:04 -0600

    Integrated Client and Server BBB

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = '91c6ddd2e86d260f186770ea908f0f7cf8a49653'
+AESD_ASSIGNMENTS_VERSION = 'a987198d552d27ffcc9a15340657a04ba9a31f61'
 AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-5013/final-project-sarayumanagoli.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 
@@ -15,14 +15,11 @@ define AESD_ASSIGNMENTS_BUILD_CMDS
 endef
 
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/helloworld $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/sharedmem $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/server $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/producer $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/consumer $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/servertest $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/clienttest $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/TMP102 $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/gassensor $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/led $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/receiver $(TARGET_DIR)/bin
 endef
 
 
